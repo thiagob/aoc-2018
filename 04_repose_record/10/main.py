8b3687ada57c4ad5881bf57408f8fa2a53849726
Bohn <thiago.bohn@sap.com>
2018-12-05 22:13:19 -0200

    Day 4 First Star

--- 04_repose_record/10/main.py
+++ 04_repose_record/10/main.py
@@ -19,38 +19,38 @@ def execute(data):
             if guard not in guards:
                 guards[guard] = {
                     "splet_at": 0,
-                    "minutes_slepping": 0,
-                    "records": [0 for x in range(60)]
+                    "records": [0 for x in range(59)],
+                    "total_asleep": 0,
                 }
 
         if line.endswith('falls asleep'):
             guards[guard]["splet_at"] = minutes
 
         if line.endswith('wakes up'):
-            guards[guard]["minutes_slepping"] += minutes - \
-                guards[guard]["splet_at"]
+            slepping = minutes - guards[guard]["splet_at"]
+            guards[guard]["total_asleep"] += slepping
 
-            for i in range(guards[guard]["splet_at"], minutes):
-                guards[guard]["records"][i] += 1
+            for x in range(guards[guard]["splet_at"], minutes):
+                guards[guard]["records"][x] += 1
 
             guards[guard]["splet_at"] = 0
 
-    # find worst guard
-    max = 0
-    worst_guard = 0
-    for g in guards:
-        if guards[g]["minutes_slepping"] > max:
-            worst_guard = g
 
-    max = 0
-    worst_minute = 0
-    for m in range(0,60):
-        if guards[worst_guard]["records"][m] > max:
-            max = guards[worst_guard]["records"][m]
-            worst_minute = m
+    maximum = 0
+    best_guard = 0
+    for guard in guards:
+        if guards[guard]["total_asleep"] > maximum:
+            maximum = guards[guard]["total_asleep"]
+            best_guard = guard            
 
+    maximum = 0
+    best_minute = 0
+    for idx, val in enumerate(guards[best_guard]["records"]):
+        if val > maximum:
+            maximum = val
+            best_minute = idx
 
-    print int(worst_guard) * worst_minute
+    print int(best_guard) * best_minute
 
 
 f = open("./04_repose_record/10/input.txt", "r")
