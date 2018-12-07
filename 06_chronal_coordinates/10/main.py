a2282660d1bfe60c7d6f794e07748a6784df70d8
Bohn <thiago.bohn@sap.com>
2018-12-07 17:50:04 -0200

    Day 6 Part 1

--- 06_chronal_coordinates/10/main.py
+++ 06_chronal_coordinates/10/main.py
@@ -1,7 +1,6 @@
 import re
 
 coordinates = []
-areas = []
 
 def manhattan_distance(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
@@ -18,9 +17,21 @@ def calculate_closest(position):
     else:
         return distances.index(min_dist)
 
+def export(grid):
+    file = open('./06_chronal_coordinates/10/export.txt', 'w')
+    
+    for c in range(0, len(grid)):
+        for r in range(0, len(grid)):
+            file.write(str(grid[r][c]) + '\t')
+        file.write('\n')
+    file.close()
+
 def execute(data):
     grid_size = max([int(s) for s in re.findall(r'\d+', "".join(data))]) + 1
-    grid = [['.' for x in range(grid_size)] for y in range(grid_size)]
+    grid = [[-1 for x in range(grid_size)] for y in range(grid_size)]
+
+    areas = []
+    infinites = {}
 
     for item in data:
         coordinates.append([int(s) for s in re.findall(r'\d+', item)])
@@ -34,11 +45,14 @@ def execute(data):
                 grid[r][c] = closest
                 
                 is_infinite = (c <= 0 or r <= 0 or c >= grid_size or r >= grid_size)
-                if not is_infinite:
+                if is_infinite:
+                    infinites[closest] = True
+                    areas[closest] = 0
+                elif closest not in infinites:
                     areas[closest] += 1
 
     print max(areas)
-
+    export(grid)
 
 f = open("./06_chronal_coordinates/input.txt", "r")
 data = f.readlines()
